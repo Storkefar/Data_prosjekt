8781d6fd0eddf81dbe167af2dbd5f617e738ce8b
Storkefar <95031716+Storkefar@users.noreply.github.com>
2022-05-05 15:23:32 +0200

    Update zumo.ino

--- zumo.ino
+++ zumo.ino
@@ -6,12 +6,32 @@ const int ENB = 10; //Motor 2
 const int IN2 = 16; //Wheel direction 2
 
 bool mode=true; //Variabel som bestemmer om bilen skal kjøre fort eller sakte
-int motorL=200; //Variabel som bestemmer farten til det venstre hjulet
-int motorR=200; //Variabelen som bestemmer farten til det høyre hjulet
+int motorL=0; //Variabel som bestemmer farten til det venstre hjulet
+int motorR=0; //Variabelen som bestemmer farten til det høyre hjulet
+char kommando;
 
 Zumo32U4Encoders encoders;//Gir encoders en class
 Zumo32U4Motors motors; //Gir motors en class
 Zumo32U4LineSensors lineSensors; //Gir lineSensors en class
+
+//globale variabler til dishcarge funksjonen
+int hiddenChargeTotalTime = 0;
+int emergencyChargeStatus = 0; 
+//globale variabler til regularCharge funksjonen
+
+//globale variabler flere funksjoner er avhengig av
+
+float batteryCapacityAndPercentage[2] = {2200*60*60*1000,100};
+bool discharge = true;
+unsigned long chargingFirstTime = 0;
+int timeInterval = 100;
+unsigned long dischargeFirstTime = 0;
+int batteryHealth = 100;
+int batteryHealthLevel = 4;
+
+//globale variabler til batteryWarning funksjonen
+unsigned long warningFirstTime = 0;
+
 #define NUM_SENSORS 5 //Definerer 5 til NUM_SENSORS
 unsigned int lineSensorValues[NUM_SENSORS]; //Lager et array for de fem Sensor verdiene 
 
@@ -22,12 +42,15 @@ float averageSpeedMaxSpeedTime [3]; // variabler gjennomsnittsutregning og div
 float averageSpeedFirstDistance;
 float maxSpeed;
 float maxSpeedTime;
-unsigned long counter;
 unsigned long lastTime;
 char sendData; 
 bool readData =false;
+int counter;
 
 int fart = 200; //Definerer fart veriablen som senere vil bli brukt til å styre farten på motorene 
+
+int soppelKasse=-1;
+char soppelstate[4];
 
 void setup() {
    lineSensors.initFiveSensors(); // Skrur på alle 5 IR-sensorene
@@ -40,7 +63,6 @@ void setup() {
 void loop() {
   String topic="";
   String msg="";//Variabel som brukes til å velge hvilken switch case meldingen skal trigge
-  char kommando;
   
   while (Serial1.available() > 0) {
     
@@ -71,7 +93,7 @@ void loop() {
       //Serial.println(topic);
       //Serial.println(kommando); 
     }
-    Serial.println(topic);
+    //Serial.println(topic);
     if(topic == "fj/kontroll") {
       kommando = msg[0];
      }
@@ -87,8 +109,25 @@ void loop() {
       readData=false;
       
       }
+    if (topic == "soppelstate") {
+      for (int i = 0; i < 4; i++) {
+        soppelstate[i] = msg[i];
+        Serial.println(soppelstate[i]);  
+      }
+      if (soppelstate[soppelKasse] == '0') {
+          motors.setSpeeds(200, 200);
+          delay(250);
+          kommando = 'l';
+        }
+      else if (soppelstate[soppelKasse] == '1') {
+        soppelstate[soppelKasse] == '0';
+        soppelHent();
+        kommando='l';
+        }
+    }
 
     //kommando flow -------------------------------------------------------------------------
+    //Serial.println(kommando);
     switch(kommando) {
       case 'w': //case for at bilen skal kjøre fremover
         motorR=fart;
@@ -116,12 +155,10 @@ void loop() {
       break;
       
       case 'c': //case for å kallibrere linjefølgnings sensorene
+        kommando=' ';
         calibrateSensors();
       break;
       
-      case 'l': //case for å starte linjefølging
-        linjefolg();
-      break;
 
       case 'q':// case for at bilen skal snu seg på samme sted mot venstre
         motorR=fart;
@@ -148,15 +185,23 @@ void loop() {
     motors.setSpeeds(motorL,motorR); // Setter motor farten til det den ble definert som i casene.
     //-----------------------------------------------------------------------------------------------------------------------------
   }
+   if(kommando == 'l') {
+   linjefolg(kommando);
+   }
+   if(kommando=='o'){
+    Serial1.println("soppel!fetch soppel");
+    kommando=' '; 
+    motorR = 0;
+    motorL = 0;
+   }
+   motors.setSpeeds(motorL,motorR); // Setter motor farten til det den ble definert som i casene.
    hastighet(sendData, readData);
 }
   
 void hastighet(char sendData, bool readData){ // Funksjon som oppdaterer arrayen med hastighet og distanse i tillegg regner ut visse verdier etter 60sek
-  float timeInterval = 100; //setter tidsintervallet
+
 
   unsigned long lastTime = millis(); //måler nåværende tid
-  //Serial.println(sendData);
-  //Serial.println(readData);
 
   if ((lastTime - firstTime) >= timeInterval){ //regner ut fart og distanse hvis gitt tidsintervall har passert
     float countRight = encoders.getCountsAndResetRight();
@@ -165,7 +210,7 @@ void hastighet(char sendData, bool readData){ // Funksjon som oppdaterer arrayen
     float revolutions = averageCount/909.7;
     float distanceInCm = 12.25 * revolutions;
 
-    speedAndDistanceArray[0] = distanceInCm / (timeInterval/1000); //lagrer farten i arrayen
+    speedAndDistanceArray[0] = distanceInCm / (0.1); //lagrer farten i arrayen
     speedAndDistanceArray[1] += abs(distanceInCm); //lagrer distansen i arrayen
 
     if (abs(speedAndDistanceArray[0]) > maxSpeed){ //sjeker om vi har en ny maksfart
@@ -190,20 +235,19 @@ void hastighet(char sendData, bool readData){ // Funksjon som oppdaterer arrayen
           
         }
       }
-    
-    counter += timeInterval/1000; //oppdaterer telleren som kan fortelle oss når det har gått 60 sekunder
-
     firstTime = millis(); //Starter ny tidtaking
     
     }
 
 
 
-    if ((counter % 5)==0){
+    if (counter % (60*1000)==0){
       averageSpeedMaxSpeedTime[0] = (speedAndDistanceArray[1] - averageSpeedFirstDistance)/60; //regner ut gjennomsnittshastighet i cm/s ila siste 60 sek og lagrer
       averageSpeedMaxSpeedTime[1] = maxSpeed; //lagrer max hastighet
       averageSpeedMaxSpeedTime[2] = maxSpeedTime; //lagrer hvor lenge bilen har kjørt mer enn 70% av maks hastighet
-      //Serial.println("test");
+      if ((averageSpeedMaxSpeedTime[0] >= 40) || (abs(averageSpeedMaxSpeedTime[1]) >= 65) || (averageSpeedMaxSpeedTime[2] >= 30)){ //oppdaterer battery health etter krav
+        batteryHealth--;
+      }
       if(readData=true){
         if(sendData=='p'){
           String gjennomCms= String(averageSpeedMaxSpeedTime[0],2);
@@ -226,7 +270,7 @@ void calibrateSensors(){
   /*Kalliberingen av IR-sensorene gjøres slik at sensorene vet forskjellen på hva som er vanlig bakke og hva som er definert som linje. 
   Det er derfor viktig at bilen er plassert over en linje før man starter denne delen av programmet.  */
   delay(1000);//Kunstpause
-  for(uint16_t i = 0; i < 120; i++){//For-loop for at bilen skal snurre mot høyre og venstre på samme sted
+  for(uint16_t i = 0; i < 122; i++){//For-loop for at bilen skal snurre mot høyre og venstre på samme sted
     if (i > 30 && i <= 90){
       motors.setSpeeds(-200, 200);//Først mot venstre
     }
@@ -237,21 +281,61 @@ void calibrateSensors(){
   }
   motors.setSpeeds(0, 0);// Tilslutt setter vi motorene til null slik at bilen stopper og er klar til å kjøres.
 }
-void linjefolg(){
-  while (Serial1.read()==-1){ 
-         
-       int position = lineSensors.readLine(lineSensorValues); 
-         position=position-2000;
-         Serial.println(lineSensorValues[4]);
-         if(position<0){
-          motorR=-200*exp(-0.0025*abs(position))+400;
-          motorL=200*exp(-0.0025*abs(position));
-          motors.setSpeeds(motorL,motorR);
-          }
-          else{
-          motorL=-200*exp(-0.0025*abs(position))+400;
-          motorR=200*exp(-0.0025*abs(position));
-          motors.setSpeeds(motorL,motorR);
-          } 
-}
+void linjefolg(char kommando){
+  if (kommando == 'l') {
+    int position = lineSensors.readLine(lineSensorValues); 
+    position=position-2000;
+    Serial.println(lineSensorValues[4]);
+    if(position<0){
+    motorR=-200*exp(-0.0025*abs(position))+400;
+    motorL=200*exp(-0.0025*abs(position));
+    motors.setSpeeds(motorL,motorR);
+    }
+    else{
+    motorL=-200*exp(-0.0025*abs(position))+400;
+    motorR=200*exp(-0.0025*abs(position));
+    motors.setSpeeds(motorL,motorR);
+    }   
+  }
 } 
+void soppelHent(){
+  int teller = 0;
+motors.setSpeeds(-200,200);
+  delay(420);
+  motors.setSpeeds(200,200);
+  delay(200);
+  int position = lineSensors.readLine(lineSensorValues);
+  while(lineSensorValues[0] < 700) {
+    position = lineSensors.readLine(lineSensorValues); 
+    position=position-2000;
+    //Serial.println(lineSensorValues[4]);
+    if(position<0){
+    motorR=-200*exp(-0.0025*abs(position))+400;
+    motorL=200*exp(-0.0025*abs(position));
+    motors.setSpeeds(motorL,motorR);
+    }
+    else{
+    motorL=-200*exp(-0.0025*abs(position))+400;
+    motorR=200*exp(-0.0025*abs(position));
+    motors.setSpeeds(motorL,motorR);
+    }
+    if(teller == 0) {
+      if (position == -2000 || position == 2000) {
+        motors.setSpeeds(200, -200); 
+        delay(880);
+        teller++;
+      } 
+    }
+    else if (teller == 1) {
+      if (position == -2000 || position == 2000) {
+        motors.setSpeeds(200,200);
+        delay(300);
+        break;
+      }
+    }
+  }
+  motors.setSpeeds(-200,200);
+  delay(440);
+  motors.setSpeeds(200,200);
+  delay(250);
+}
